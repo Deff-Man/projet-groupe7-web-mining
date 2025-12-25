61ad448a35278f680b44db69fbe7391d07948511
Zoe-van2610 <zoe.vandevelde@student.uclouvain.be>
2025-12-25 11:24:54 +0100

    Update_put_together
    Update script Zoe_scraping

--- put_together.py
+++ put_together.py
@@ -5,9 +5,8 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
-
 # Import scrapping modules
-from AirBNB import run_airbnb 
+from AirBNB import run_airbnb
 from AppleArcade import run_apple_arcade
 from AppleMusic import run_apple_music
 from AppleOne import run_apple_one
@@ -18,13 +17,7 @@ from Pinterest import scrape_pinterest_final
 from ProtonMail import run_ProtonMail
 from Telegram import scrape_telegram_faq
 
-def main():
-
-    # Define the output and file path
-
-    OUTPUT_FOLDER = r"C:\Users\Utilisateur\Desktop\Master 1\Q1 part 2\Web mining\projet"
-    os.makedirs(OUTPUT_FOLDER, exist_ok=True)
-    FINAL_OUTPUT_FILE = os.path.join(OUTPUT_FOLDER, "faq_commun.csv")
+def run_zoe():
 
     # Configure Chrome options
     options = Options()
@@ -49,7 +42,6 @@ def main():
         ("Pinterest", scrape_pinterest_final),
         ("ProntonMail", run_ProtonMail),
         ("Telegram", scrape_telegram_faq)]
-
     try:
         # Loop through each scraper to collect data
         for name, func in scrapers:
@@ -57,7 +49,7 @@ def main():
             try:
                 # Execute the scrapping function
                 if name == "Github" or name == "AppleOne" or name == "Asana" or name == "Pinterest" or name == "Telegram" :
-                    df = func() # On appelle sans driver
+                    df = func()
                 else:
                     df = func(driver)
                 # Check if data was successfully collected
@@ -73,25 +65,8 @@ def main():
 
         # Data saving
         if all_dataframes:
-
-            # Concatenate all collected DataFrames
-            final_df = pd.concat(all_dataframes, ignore_index=True)
-
-            # Export to CSV
-            final_df.to_csv(FINAL_OUTPUT_FILE, index=False, encoding="utf-8-sig")
-
-            print(f"SUCCESS: Consolidated file saved at :")
-            print(f"{FINAL_OUTPUT_FILE}")
-            print(f"Total rows collected across all sites: {len(final_df)}")
-
-        else:
-            print("\nProcess finished but no data was collected from any site.")
-
-    except Exception as e:
-        print(f"\nCRITICAL ERROR during orchestration: {e}")
+            return pd.concat(all_dataframes, ignore_index=True)
+        return pd.DataFrame()
 
     finally:
         driver.quit()
-
-if __name__ == "__main__":
-    main()#code to collect
